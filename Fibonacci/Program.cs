bbfb41650f8afee1370ccf89cdb77f265b8a39a7
Steven Murray <tstevenm@gmail.com>
2021-10-10 14:40:50 -0500

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class FibonacciExample
+    {
+        public static void Main()
+        {
+            int firstNumber = 0, secondNumber = 1, nextNumber, numberOfElements;
+            Console.Write("Enter the number of elements to print: ");
+            numberOfElements = int.Parse(Console.ReadLine());
+            if (numberOfElements < 2)
+            {
+                Console.Write("Please enter a number greater than " + numberOfElements);
+            }
+            else
+            {
+                Console.Write(firstNumber + " " + secondNumber + " ");
+                for (int i = 2; i < numberOfElements; i++)
+                {
+                    nextNumber = firstNumber + secondNumber;
+                    Console.Write(nextNumber + " ");
+                    firstNumber = secondNumber;
+                    secondNumber = nextNumber;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 0, secondNumber = 1, nextNumber, numberOfElements;
+            Console.WriteLine("Enter the number of elements to Print: ");
+            numberOfElements = int.Parse(Console.ReadLine());
+            
+            if (numberOfElements < 2)
+            {
+                Console.Write("Please enter a number greater than 2");
+            }
+            else
+            {
+                Console.Write(firstNumber + " " + secondNumber + " ");
+
+                for (int i = 2; i < numberOfElements; i++)
+                {
+                    nextNumber = firstNumber + secondNumber;
+                    Console.Write(nextNumber + " ");
+                    firstNumber = secondNumber;
+                    secondNumber = nextNumber;
+                }
+            }
+        }
+    }
+}
